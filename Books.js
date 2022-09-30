3b2762c590e48bde93b90cd295b368e920e004a0
Meghsham15 <88832001+Meghsham15@users.noreply.github.com>
2022-09-30 17:46:01 +0530

    Update Books.js

--- Books.js
+++ Books.js
@@ -5,15 +5,13 @@ const books = [
     "Born": "15 October 1931",
     "Status": "Died on 27 July 2015",
     "Book Name": "My Journey", 
-    "Link": "https://www.google.com/search?q=apj+abdul+kalam&sxsrf=AOaemvJLMWbR0U35h6KfnCMl_rM1Wdflwg%3A1633020657243&ei=8epVYbuYDpuA9QPYgKnIDg&oq=apj+abdul+kalam&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBQgAEJECMgUIABCRAjIFCAAQkQIyBQgAEJECMggIABCABBCxAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgAEEcQsAM6BQguEIAEOgYIABAWEB5KBAhBGABQ2OYBWIzxAWD3jwJoBXACeACAAZADiAHzDpIBBzItMi4zLjGYAQCgAQHIAQi4AQLAAQE&sclient=gws-wiz&ved=0ahUKEwj71L2BlKfzAhUbQH0KHVhACukQ4dUDCA4&uact=5",
-  },
+},
   {
     "id": "",
     "Author": "",
     "Born": "",
     "Status": "",
     "Book Name": "", 
-    "Link": "",
   },
 
 ]
